ddb2b5772be49c10524e164d3d18b7a81eff3e31
Sentiago <arseny317@gmail.com>
2025-06-03 00:07:28 +0300

    feat: completely rewrote the diagram processing logic in live preview mode
    - Removed diagram processing using secondary observers. Now, a single global observer tracks the appearance of any elements inheriting from Element. For each such element, it checks whether it is a diagram or if a diagram exists above or below in the DOM structure.

--- src/diagram/adapters/adapters/markdown-live-preview-adapter.ts
+++ src/diagram/adapters/adapters/markdown-live-preview-adapter.ts
@@ -4,52 +4,17 @@ import { MarkdownPostProcessorContext } from 'obsidian';
 import { DiagramData } from '../../../settings/typing/interfaces';
 
 export class MarkdownLivePreviewAdapter extends BaseAdapter {
-    elementObservers = new Map<HTMLElement, MutationObserver>();
-
     constructor(diagram: Diagram) {
         super(diagram);
     }
 
-    async initialize(el: HTMLElement, context?: MarkdownPostProcessorContext) {
+    async initialize(
+        el: HTMLElement,
+        context?: MarkdownPostProcessorContext
+    ): Promise<void> {
         if (this.diagram.livePreviewObserver) {
             return;
         }
-
-        const elementObservers = new Map<HTMLElement, MutationObserver>();
-
-        const createPreviewObserver = (
-            target: HTMLElement
-        ): MutationObserver => {
-            const observer = new MutationObserver(
-                async (mutations, observer) => {
-                    for (const mutation of mutations) {
-                        const target = mutation.target as HTMLElement;
-                        if (target.tagName !== 'DIV') {
-                            continue;
-                        }
-                        const diagram = await this.checkForDiagram(target);
-                        if (!!diagram) {
-                            await this.processDiagram(diagram);
-                            observer.disconnect();
-                            elementObservers.delete(target);
-                        }
-                    }
-                }
-            );
-
-            elementObservers.set(target, observer);
-            observer.observe(target, {
-                childList: true,
-                subtree: true,
-            });
-
-            setTimeout(() => {
-                observer.disconnect();
-                elementObservers.delete(target);
-            }, 5000);
-
-            return observer;
-        };
 
         this.diagram.livePreviewObserver = new MutationObserver(
             async (mutations) => {
@@ -58,40 +23,27 @@ export class MarkdownLivePreviewAdapter extends BaseAdapter {
                         continue;
                     }
                     for (const addedNode of Array.from(mutation.addedNodes)) {
-                        const target = addedNode as HTMLElement;
-
-                        if (target.tagName !== 'DIV') {
+                        if (!(addedNode instanceof Element)) {
                             continue;
                         }
+                        const target = addedNode;
 
-                        if (
-                            !target?.matches(
-                                '.cm-preview-code-block.cm-embed-block'
-                            )
-                        ) {
-                            continue;
-                        }
-                        const diagram = await this.checkForDiagram(target);
+                        const diagram = await this.isThatADiagram(target);
                         if (diagram) {
                             await this.processDiagram(diagram);
-                            continue;
                         }
-
-                        createPreviewObserver(target);
                     }
                 }
             }
         );
 
-        const blocks = el.querySelectorAll(
+        const livePreviewBlocks = el.querySelectorAll(
             '.cm-preview-code-block.cm-embed-block'
         );
-        for (const block of Array.from(blocks)) {
-            const diagram = await this.checkForDiagram(block as HTMLElement);
+        for (const block of Array.from(livePreviewBlocks)) {
+            const diagram = await this.isThatADiagram(block);
             if (diagram) {
                 await this.processDiagram(diagram);
-            } else {
-                createPreviewObserver(block as HTMLElement);
             }
         }
 
