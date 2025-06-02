d10c9ff9d5f2854215eb2a5d211e535c6d2c289f
Sentiago <arseny317@gmail.com>
2025-06-02 23:26:00 +0300

    feat: enhance diagram search, source extraction, and rendering
    - Modified the `querySelectorWithData` method to accept `Element` objects. It now searches for diagrams both upward in the DOM hierarchy from the current element and downward if no diagram is found above.
    - Updated `lineStart` and `lineEnd` in `sourceExtractionWithContext` to exclude backticks from the final source code.
    - Completely rewrote `sourceExtractionWithoutContext`. Now it first checks for and retrieves cm-widget data from the diagram's parent. If no cm-widget is found, default data is returned. If a widget is detected, it extracts the existing diagram source code, along with its `lineStart` and `lineEnd`.
    - Removed the use of the `preserveDiagramOriginalSize` option in `initDiagramSize` due to its obsolescence.
    - Added a `data-diagram-zoom-drag-rendering-mode` data attribute in `createDiagramWrapper` to store the diagram's rendering mode.

--- src/diagram/adapters/base-adapter.ts
+++ src/diagram/adapters/base-adapter.ts
@@ -2,16 +2,17 @@ import { Diagram } from '../diagram';
 import { MarkdownPostProcessorContext } from 'obsidian';
 import { DiagramData } from '../../settings/typing/interfaces';
 import hash from 'hash.js';
+import { HTMLElementWithCMView } from './typing/interfaces';
 
 export abstract class BaseAdapter {
     constructor(protected diagram: Diagram) {}
 
     abstract initialize(
-        el: HTMLElement,
+        el: Element,
         context?: MarkdownPostProcessorContext
     ): Promise<void>;
 
-    protected async checkForDiagram(element: HTMLElement) {
+    protected async isThatADiagram(element: Element) {
         const diagram = this.querySelectorWithData(element);
 
         const svg = diagram?.element.querySelector('svg');
@@ -22,34 +23,19 @@ export abstract class BaseAdapter {
         }
     }
 
-    /**
-     * Searches for a diagram element within the provided container element.
-     *
-     * This method iterates over the list of supported diagrams and checks if
-     * the diagram is enabled. If the diagram is enabled, it searches for an
-     * element within the container element that matches the diagram's selector.
-     * If an element is found, it returns an object containing the element and
-     * the diagram data. Otherwise, it returns null.
-     *
-     * @param container - The container element to search for the diagram.
-     * @returns An object containing the diagram element and the diagram data, or
-     * null if no diagram is found.
-     */
     protected querySelectorWithData(
-        container: HTMLElement
+        element: Element
     ): { diagram: DiagramData; element: HTMLElement } | null {
         for (const diagram of this.diagram.plugin.settings.supported_diagrams) {
             if (!diagram.on) {
                 continue;
             }
 
-            const element: HTMLElement | null = container.matches(
-                diagram.selector
-            )
-                ? container
-                : container.querySelector(diagram.selector);
-            if (element) {
-                return { element, diagram };
+            const diagramElement: HTMLElement | null =
+                element.closest(diagram.selector) ||
+                element.querySelector(diagram.selector);
+            if (diagramElement) {
+                return { element: diagramElement, diagram };
             }
         }
         return null;
@@ -125,10 +111,9 @@ export abstract class BaseAdapter {
                 lineEnd: 0,
             };
         }
-
         const { lineStart: ls, lineEnd: le, text } = sectionsInfo;
-        const lineStart = ls;
-        const lineEnd = le;
+        const lineStart = ls + 1;
+        const lineEnd = le - 1;
         const lines = text.split('\n');
         const source = lines.slice(lineStart, lineEnd + 1).join('\n');
 
@@ -139,17 +124,14 @@ export abstract class BaseAdapter {
         };
     }
 
-    protected sourceExtractionWithoutContext(el: HTMLElement):
-        | {
-              source: 'No source available';
-              lineStart: 0;
-              lineEnd: 0;
-          }
-        | { source: any; lineStart: any; lineEnd: any } {
-        const e = this.diagram.plugin.context.view?.editor as unknown as any;
-        const startPos = e.cm.posAtDOM(el.parentElement);
-
-        if (!startPos) {
+    protected sourceExtractionWithoutContext(el: HTMLElement): {
+        source: string;
+        lineStart: number;
+        lineEnd: number;
+    } {
+        const parent = el.parentElement as HTMLElementWithCMView;
+        const widgetData = parent.cmView?.deco?.widget;
+        if (!widgetData) {
             return {
                 source: 'No source available',
                 lineStart: 0,
@@ -157,18 +139,10 @@ export abstract class BaseAdapter {
             };
         }
 
-        const data = this.diagram.plugin.context.view?.editor
-            .getValue()
-            .slice(startPos);
-        const source = data?.match(/^"?(```.+?```)/ms)?.[1] ?? 'No source';
-        const endPos = startPos + source.length;
-        const lineStart = e.cm.state.doc.lineAt(startPos).number;
-        const lineEnd = e.cm.state.doc.lineAt(endPos).number;
-
         return {
-            source: source,
-            lineStart: lineStart,
-            lineEnd: lineEnd,
+            source: widgetData.code,
+            lineStart: widgetData.lineStart,
+            lineEnd: widgetData.lineEnd,
         };
     }
 
@@ -178,19 +152,23 @@ export abstract class BaseAdapter {
         if (!diagramOriginalSize) {
             return false;
         }
-        let expandedWidth, expandedHeight, foldedWidth, foldedHeight;
 
-        if (this.diagram.plugin.settings.preserveDiagramOriginalSize) {
-            expandedWidth = diagramOriginalSize.width;
-            expandedHeight = diagramOriginalSize.height;
-            foldedWidth = diagramOriginalSize.width / 2;
-            foldedHeight = diagramOriginalSize.height / 2;
-        } else {
-            expandedWidth = parseInt(this.diagram.plugin.settings.diagramExpanded.width, 10);
-            expandedHeight = parseInt(this.diagram.plugin.settings.diagramExpanded.height, 10);
-            foldedWidth = parseInt(this.diagram.plugin.settings.diagramFolded.width, 10);
-            foldedHeight = parseInt(this.diagram.plugin.settings.diagramFolded.height, 10);
-        }
+        const expandedWidth = parseInt(
+            this.diagram.plugin.settings.diagramExpanded.width,
+            10
+        );
+        const expandedHeight = parseInt(
+            this.diagram.plugin.settings.diagramExpanded.height,
+            10
+        );
+        const foldedWidth = parseInt(
+            this.diagram.plugin.settings.diagramFolded.width,
+            10
+        );
+        const foldedHeight = parseInt(
+            this.diagram.plugin.settings.diagramFolded.height,
+            10
+        );
 
         this.diagram.size = {
             expanded: {
@@ -218,6 +196,13 @@ export abstract class BaseAdapter {
         const el = diagram.element;
 
         container.addClass('diagram-container');
+        const renderingMode = this.diagram.plugin.isInPreviewMode
+            ? 'preview'
+            : 'live-preview';
+        container.setAttribute(
+            'data-diagram-zoom-drag-rendering-mode',
+            `${renderingMode}`
+        );
         el.parentNode?.insertBefore(container, el);
         container.appendChild(el);
 
