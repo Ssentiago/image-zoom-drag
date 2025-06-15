3c4069b13b26cc8b8aefbcea9e89fad7a1b1e17f
Sentiago <arseny317@gmail.com>
2025-06-15 20:02:44 +0300

    refactor: renamed component

--- src/settings/components/SettingsRoot.tsx
+++ src/settings/components/SettingsRoot.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 
 import { App } from 'obsidian';
-import { SettingProvider } from './core/context';
+
 import DiagramZoomDragPlugin from '../../core/diagram-zoom-drag-plugin';
+import { SettingProvider } from './core/SettingsContext';
 import SettingsPage from './settings-page/SettingsPage';
-import styled, { keyframes } from 'styled-components';
 
-const fadeInSlide = keyframes`
-    from {
-        opacity: 0;
-        transform: translateY(-8px);
-    }
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
-`;
-
-const SettingsContainer = styled.div`
-    animation: ${fadeInSlide} 0.3s cubic-bezier(0.4, 0, 0.2, 1);
-`;
 /**
  * The root component of the settings UI.
  *
@@ -29,15 +15,16 @@ const SettingsContainer = styled.div`
  * @param app The Obsidian app instance.
  * @param plugin The instance of the DiagramZoomDragPlugin.
  */
-const Application: React.FC<{
+const SettingsRoot: React.FC<{
     app: App;
     plugin: DiagramZoomDragPlugin;
 }> = ({ app, plugin }) => (
-    <SettingProvider app={app} plugin={plugin}>
-        <SettingsContainer>
-            <SettingsPage />
-        </SettingsContainer>
+    <SettingProvider
+        app={app}
+        plugin={plugin}
+    >
+        <SettingsPage />
     </SettingProvider>
 );
 
-export default Application;
+export default SettingsRoot;
