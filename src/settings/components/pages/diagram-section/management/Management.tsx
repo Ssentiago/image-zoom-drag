5962226ad9c0f548ef332682e9fb246a476ec661
Sentiago <arseny317@gmail.com>
2025-06-15 20:32:34 +0300

    feat: rewrite Management component to use sub-components and providers
    - Extract code into sub-components
    - Use DiagramManagerProvider for diagram data management
    - Use DiagramHistoryProvider for diagram history management

---
+++ src/settings/components/pages/diagram-section/management/Management.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+
+import AddNewDiagram from './add-new-diagram/AddNewDiagram';
+import AvailableDiagrams from './available-diagrams/AvailableDiagrams';
+import {
+    DiagramManagerProvider,
+    useDiagramManagerContext,
+} from './context/DiagramManagerContext';
+import { DiagramHistoryProvider } from './context/HistoryContext';
+
+const Management: FC = () => {
+    const { diagrams, saveDiagrams } = useDiagramManagerContext();
+
+    return (
+        <DiagramManagerProvider>
+            <DiagramHistoryProvider
+                state={diagrams}
+                updateState={saveDiagrams}
+            >
+                <AddNewDiagram />
+                <AvailableDiagrams />
+            </DiagramHistoryProvider>
+        </DiagramManagerProvider>
+    );
+};
+
+export default Management;
