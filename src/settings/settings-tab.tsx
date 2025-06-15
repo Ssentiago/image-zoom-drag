d9198ffdd54f9bb7f6320e3aa205a9703293012e
Sentiago <arseny317@gmail.com>
2025-06-15 20:01:59 +0300

    feat: updated SettingsTab
    - Auto-cleanup of event listeners when closing the settings tab
    - Removed unnecessary div-wrapper for react-root

--- src/settings/settings-tab.tsx
+++ src/settings/settings-tab.tsx
@@ -1,11 +1,12 @@
 import { App, PluginSettingTab } from 'obsidian';
-import DiagramZoomDragPlugin from '../core/diagram-zoom-drag-plugin';
 import { createRoot, Root } from 'react-dom/client';
-import Application from './components/Application';
-import React from 'react';
+
+import DiagramZoomDragPlugin from '../core/diagram-zoom-drag-plugin';
+import SettingsRoot from './components/SettingsRoot';
 
 export class SettingsTab extends PluginSettingTab {
     private root: Root | undefined = undefined;
+
     constructor(
         public app: App,
         public plugin: DiagramZoomDragPlugin
@@ -14,18 +15,14 @@ export class SettingsTab extends PluginSettingTab {
         this.containerEl.addClass('diagram-zoom-drag-settings');
     }
 
-    /**
-     * Displays the settings tab.
-     *
-     * This method creates a new root element within the container and renders
-     * the React application with the provided app and plugin properties.
-     *
-     * @returns A promise that resolves when the display is complete.
-     */
-    async display(): Promise<any> {
-        const root = this.containerEl.createDiv();
-        this.root = createRoot(root);
-        this.root.render(<Application app={this.app} plugin={this.plugin} />);
+    display(): void {
+        this.root = createRoot(this.containerEl);
+        this.root.render(
+            <SettingsRoot
+                app={this.app}
+                plugin={this.plugin}
+            />
+        );
     }
 
     /**
@@ -34,6 +31,8 @@ export class SettingsTab extends PluginSettingTab {
      * This method unmounts the React root component and clears the container element.
      */
     hide(): void {
+        this.plugin.settings.eventBus.removeAllListeners();
+
         this.root?.unmount();
         this.containerEl.empty();
     }
