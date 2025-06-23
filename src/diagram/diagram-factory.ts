db02d5eded187f1d2efaadf340313423158801e3
Sentiago <arseny317@gmail.com>
2025-06-23 19:30:06 +0300

    feat: update diagram factory to support unified context object

--- src/diagram/diagram-factory.ts
+++ src/diagram/diagram-factory.ts
@@ -1,35 +1,15 @@
 import DiagramZoomDragPlugin from '../core/diagram-zoom-drag-plugin';
-import Diagram from './diagram';
-import {
-    BaseDiagramContext,
-    DiagramContext,
-    DiagramSize,
-    FileStats,
-    SourceData,
-} from './types/interfaces';
+import InteractiveElement from './interactiveElement';
+import { DiagramContext, FileStats } from './types/interfaces';
 
 export default class DiagramFactory {
     static createDiagram(
         plugin: DiagramZoomDragPlugin,
-        diagramDescriptor: BaseDiagramContext,
-        sourceData: SourceData,
-        size: DiagramSize,
-        fileStats: FileStats,
-        container: HTMLElement
-    ): Diagram {
-        const extendedDiagramDescriptor = {
-            ...diagramDescriptor,
-            sourceData,
-            size,
-        } as DiagramContext;
+        context: DiagramContext,
+        fileStats: FileStats
+    ): InteractiveElement {
         plugin.logger.debug('Creating diagram...');
-        const diagram = new Diagram(
-            plugin,
-            container,
-            extendedDiagramDescriptor,
-            fileStats
-        );
-        diagram.initialize();
+        const diagram = new InteractiveElement(plugin, context, fileStats);
         plugin.logger.debug(
             'Diagram was created and initialized successfully.'
         );
