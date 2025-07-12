6b36826ade53dc549a3cdaf6a3c07adab485fef9
Sentiago <arseny317@gmail.com>
2025-07-13 00:01:12 +0300

    feat: rewrite context menu
    - Add new options (copy as..., export as...)
    - Context menu now show conditionally, bases on user settings

--- src/modes/integrated-mode/interactify-unit/events/handlers/context-menu/context-menu.ts
+++ src/modes/integrated-mode/interactify-unit/events/handlers/context-menu/context-menu.ts
@@ -2,29 +2,29 @@ import { Component, Menu } from 'obsidian';
 
 import Events from '../../events';
 import { Handler } from '../../types/interfaces';
-import { CopyUnit } from './context-actions/copy-unit';
-import { CopyUnitSource } from './context-actions/copy-unit-source';
+import { Copy } from './context-actions/copy';
 import { Export } from './context-actions/export';
 import Info from './context-actions/info/info';
+import OpenInPopup from './context-actions/open-in-popup';
 
 export class ContextMenu extends Component implements Handler {
     private readonly export: Export;
-    private readonly copy: CopyUnit;
-    private readonly copySource: CopyUnitSource;
+    private readonly copy: Copy;
     private readonly info: Info;
+    private readonly openInPopup: OpenInPopup;
 
     constructor(public readonly events: Events) {
         super();
 
         this.export = new Export(this);
-        this.copy = new CopyUnit(this);
-        this.copySource = new CopyUnitSource(this);
+        this.copy = new Copy(this);
         this.info = new Info(this);
+        this.openInPopup = new OpenInPopup(this);
 
         this.addChild(this.export);
         this.addChild(this.copy);
-        this.addChild(this.copySource);
         this.addChild(this.info);
+        this.addChild(this.openInPopup);
     }
 
     initialize(): void {
@@ -32,47 +32,128 @@ export class ContextMenu extends Component implements Handler {
 
         const { container } = this.events.unit.context;
 
-        this.registerDomEvent(container, 'contextmenu', this.onContextMenu, {
-            capture: true,
-            passive: false,
-        });
+        this.registerDomEvent(
+            container,
+            'contextmenu',
+            (e) => {
+                const settings = this.events.unit.plugin.settings.$;
+
+                const origin = this.events.unit.context.origin;
+
+                if (
+                    !settings.units.contextMenu.showForDiagrams &&
+                    origin === 'generated'
+                ) {
+                    return;
+                }
+
+                if (
+                    !settings.units.contextMenu.showForOtherImages &&
+                    origin !== 'generated'
+                ) {
+                    return;
+                }
+
+                e.preventDefault();
+                e.stopPropagation();
+
+                this.onContextMenu(e);
+            },
+            {
+                capture: true,
+                passive: false,
+            }
+        );
     }
 
     private readonly onContextMenu = (event: MouseEvent) => {
         const { element } = this.events.unit.context;
 
-        event.preventDefault();
-        event.stopPropagation();
-
-        const isThisSvg = element.matches('svg');
-
         this.events.unit.context.content.focus();
+        this.showMenu(event, element);
+    };
 
+    showMenu(event: MouseEvent, element: HTMLImageElement | SVGElement): void {
+        const menu = this.createMenu(element);
+        menu.showAtMouseEvent(event);
+    }
+
+    createMenu(element: HTMLImageElement | SVGElement) {
         const menu = new Menu();
 
         menu.addItem((item) => {
             item.setIcon('download');
-            item.setTitle('Export as image');
-            item.onClick(async () => {
-                await this.export.export();
+            item.setTitle('Export as...');
+
+            const submenu = item.setSubmenu();
+
+            submenu.addItem((item) => {
+                item.setIcon('file-export');
+                item.setTitle('PNG');
+                item.onClick(async () => {
+                    await this.export.exportAsPNG(element);
+                });
             });
+
+            submenu.addItem((item) => {
+                item.setIcon('image');
+                item.setTitle('JPG');
+                item.onClick(async () => {
+                    await this.export.exportAsJPG(element);
+                });
+            });
+
+            element instanceof SVGElement &&
+                submenu.addItem((item) => {
+                    item.setIcon('file-code');
+                    item.setTitle('SVG');
+                    item.onClick(async () => {
+                        await this.export.exportAsSVG(element);
+                    });
+                });
         });
 
         menu.addItem((item) => {
             item.setIcon('copy');
-            item.setTitle(`Copy ${!isThisSvg ? 'as image' : 'as SVG code'}`);
-            item.onClick(async () => {
-                await this.copy.copy();
+            item.setTitle(`Copy as...`);
+
+            const submenu = item.setSubmenu();
+
+            submenu.addItem((item) => {
+                item.setIcon('image');
+                item.setTitle('PNG');
+                item.onClick(async () => {
+                    await this.copy.copyAsPNG(element);
+                });
+            });
+
+            element instanceof SVGElement &&
+                submenu.addItem((item) => {
+                    item.setIcon('code-2');
+                    item.setTitle('Plain code');
+                    item.onClick(async () => {
+                        await this.copy.copyAsPlain(element);
+                    });
+                });
+            submenu.addItem((item) => {
+                item.setIcon('file-code');
+                item.setTitle('Source');
+                item.onClick(async () => {
+                    await this.copy.copyAsSource();
+                });
             });
         });
 
-        menu.addItem((item) => {
-            item.setIcon('file-text');
-            item.setTitle('Copy as source');
-            item.onClick(async () => {
-                await this.copySource.copy();
+        menu.addSeparator();
+
+        !(element instanceof SVGElement) &&
+            menu.addItem((item) => {
+                item.setIcon('maximize-2');
+                item.setTitle('Open this image in popup window');
+                item.onClick(() => {
+                    this.openInPopup.openPopupForImage(element);
+                });
             });
-        });
 
         menu.addItem((item) => {
             item.setIcon('info');
@@ -82,6 +163,6 @@ export class ContextMenu extends Component implements Handler {
             });
         });
 
-        menu.showAtMouseEvent(event);
-    };
+        return menu;
+    }
 }
